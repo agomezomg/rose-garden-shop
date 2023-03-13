f086725ae08078cc935f1fdcc4f97eca1146eb01
Ana <anagomez98@unitec.edu>
2023-03-12 21:03:57 -0600

    adding test cases

--- JavaScript-Jest/tests/rose_garden.test.js
+++ JavaScript-Jest/tests/rose_garden.test.js
@@ -1,29 +1,59 @@
 const {Shop, Item} = require("../src/rose_garden");
+const items = [
+  new Item("+5 Dexterity Vest", 10, 20),
+  new Item("Aged Brie", 2, 0),
+  new Item("Elixir of the Mongoose", 5, 7),
+  new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+];
+
+const roseGarden = new Shop(items);
+let days = 1;
 
 describe("Rose Garden", function() {
-  it("should foo", function() {
-    const items = [
-      new Item("+5 Dexterity Vest", 10, 20),
-      new Item("Aged Brie", 2, 0),
-      new Item("Elixir of the Mongoose", 5, 7),
-      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-    ];
-    const roseGarden = new Shop([new Item("foo", 0, 0)]);
-    console.log(roseGarden);
+  do {
+    roseGarden.updateQuality();
+    days--;
+  } while (days > 0);
 
-    let days = 15;
-
-    do {
-      roseGarden.updateQuality();
-      roseGarden.items.forEach((item) => {
-        
-      })
-      days--;
-    } while (days > 0);
-    expect(items[0].name).toBe("foo");
+  it("should validate that quality is greater than 0", function() {
+    roseGarden.items.forEach((item) => {
+      expect(item.quality).toBeGreaterThan(-1)
+    })
   });
+
+  it("should validate that quality is 80 or no greater than 50", function() {
+    roseGarden.items.forEach((item) => {
+      if (item.name !== 'Sulfuras, Hand of Ragnaros')
+        expect(item.quality).toBeLessThan(51);
+      else expect(item.quality).toBe(80);
+    })
+  })
+
+  it("regular items should decrement per day", function() {
+    roseGarden.items.forEach((item) => {
+      switch (item.name) {
+        case 'Backstage passes to a TAFKAL80ETC concert':
+        case 'Aged Brie':
+        case 'Sulfuras, Hand of Ragnaros':
+          break;
+        default:
+          expect(item.quality).toBe(item.quality - days)
+      }
+    })
+  })
+
+  it("validate accurate quality for backstage passes", function() {
+    roseGarden.items.forEach((item) => {
+      if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.sellIn < 0)
+          expect(item.quality).toBe(0);
+        else if (item.sellIn > 10)
+          expect(item.quality).toBe(item.quality + days)
+        }
+    })
+  })
 });
